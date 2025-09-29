7f05b80235d2fdd94439d725db142da9760e7708
vivek <hegdevivek379@gmail.com>
2025-09-29 21:20:25 +0530

    the url error solved

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 # Inside config.py
-GEMINI_API_KEY = "AIzaSyAQjPCv-zXywPTd5mVfeqMXjdYe0DImi50"
+
 POLYGON_API_KEY = "iNpbhWy5OUZoCPldkmdIMeg4iiQZPiGN"
+GEMINI_API_KEY = "AIzaSyDglS0iEkKbiFpqqyjI2ctLEENkhEuD8Ro"
